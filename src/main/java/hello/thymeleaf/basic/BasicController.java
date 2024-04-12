ea486aec6ebbe49ceae925ed7b7491f032daeaea
yongjin <dydwls2270@naver.com>
2024-04-12 14:43:33 +0900

    day2-2

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -114,6 +114,26 @@ public class BasicController {
         return "basic/condition";
     }
 
+    @GetMapping("/comments")
+    public String comments(Model model) {
+        model.addAttribute("data", "Spring!");
+
+        return "basic/comments";
+    }
+
+    @GetMapping("/block")
+    public String block(Model model) {
+        addUser(model);
+        return "basic/block";
+    }
+
+    @GetMapping("/javascript")
+    public String javascript(Model model) {
+        model.addAttribute("user", new User("userA", 10));
+        addUser(model);
+        return "basic/javascript";
+    }
+
     public void addUser(Model model) {
         List<User> list = new ArrayList<>();
         list.add(new User("userA",10));
